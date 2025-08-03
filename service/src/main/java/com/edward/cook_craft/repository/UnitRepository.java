8e897637a9b8237ca1bfd0b589b0abb99a2a26f3
hoangfcoongminh <hoangfcoongminh@gmail.com>
2025-08-03 14:10:16 +0700

    update unit controller and fix authentic service

--- service/src/main/java/com/edward/cook_craft/repository/UnitRepository.java
+++ service/src/main/java/com/edward/cook_craft/repository/UnitRepository.java
@@ -2,9 +2,14 @@ package com.edward.cook_craft.repository;
 
 import com.edward.cook_craft.model.Unit;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface UnitRepository extends JpaRepository<Unit, Long> {
 
@@ -13,4 +18,23 @@ public interface UnitRepository extends JpaRepository<Unit, Long> {
             "WHERE u.id = :id " +
             "AND u.status = 1")
     boolean existsById(@NotNull @Param("id") Long id);
+
+    @Query(value = "SELECT COUNT(u) > 0 " +
+            "FROM Unit u " +
+            "WHERE u.name = :name " +
+            "AND u.status = 1")
+    boolean existsByName(@NotNull @Param("name") String name);
+
+    @Query(value = "SELECT u " +
+            "FROM Unit u " +
+            "WHERE (:name IS NULL OR LOWER(u.name) LIKE CONCAT('%', :name, '%')) " +
+            "AND u.status = 1")
+    Page<Unit> filter(@Param("name") String name, Pageable pageable);
+
+    @NotNull
+    @Query(value = "SELECT u " +
+            "FROM Unit u " +
+            "WHERE u.id = :id " +
+            "AND u.status = 1")
+    Optional<Unit> findById(@NotNull @Param("id") Long id);
 }
